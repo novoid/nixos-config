dd68aa7f2d93eb5d9a9f1f5758c7cee7dde4f19b
Karl Voit <git@Karl-Voit.at>
2023-08-06 16:13:03 +0200

    moved xfce packages from homemanager/xfce.nix to configuration.nix; fixes #2

--- homemanager/xfce.nix
+++ homemanager/xfce.nix
@@ -16,43 +16,6 @@
     
     home.packages = with pkgs; [
 
-      redshift
-      xclip
-      xfce.xfce4-dict
-      autokey
-      gnome-icon-theme
-      dejavu_fonts
-      gentium
-      yanone-kaffeesatz
-      wmctrl
-      gnome.gnome-keyring
-      xfce.xfwm4-themes
-      xfce.thunar
-      xfce.thunar-volman
-      xfce.xfce4-appfinder
-      xfce.xfce4-clipman-plugin
-      xfce.xfce4-cpugraph-plugin
-      xfce.xfce4-eyes-plugin
-      xfce.xfce4-whiskermenu-plugin
-      xfce.xfce4-weather-plugin
-      xfce.xfce4-mailwatch-plugin
-      xfce.xfce4-netload-plugin
-      xfce.xfce4-notes-plugin
-      xfce.xfce4-notifyd
-      xfce.xfce4-power-manager
-      xfce.xfce4-pulseaudio-plugin
-      xfce.xfce4-screensaver
-      xfce.xfce4-screenshooter
-      xfce.xfce4-session
-      xfce.xfce4-settings
-      xfce.xfce4-systemload-plugin
-      xfce.xfce4-taskmanager
-      xfce.xfce4-terminal
-      xfce.xfce4-verve-plugin
-      emojione
-      rofi
-      brightnessctl
-      # libsForQt514.oxygen-icons5 ## 2022-11-06 broken
 
     ];
 
