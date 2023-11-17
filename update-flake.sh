9eb0f8f9065104eb5c9ef7914bf7f3f744941a14
Karl Voit <karl.voit@rise-world.com>
2023-11-17 18:43:01 +0100

    update-flake.sh added git commit after update

--- update-flake.sh
+++ update-flake.sh
@@ -1,1 +1,6 @@
-nix flake update
+#!/usr/bin/env bash
+nix flake update && \
+	git add flake.lock && \
+	git commit -m 'updated flake.lock via update-flake.sh' && \
+	echo "finished without error."
+#end
