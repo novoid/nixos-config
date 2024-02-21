b8a0f77daa9eda9c02e94fc6027b3f84ae997e67
Karl Voit <karl.voit@rise-world.com>
2024-02-21 11:39:41 +0100

    qemu tool tests

--- homemanager/qemu-kvm-host.nix
+++ homemanager/qemu-kvm-host.nix
@@ -4,8 +4,11 @@
 
     home.packages = with pkgs; [
     qemu
-    virt-manager-qt
+    # virt-manager-qt   ## seems to only connect to existing VMs and not create new ones
     # broken:      aqemu
+    # qtemu  ## looks nice but is not able to find the installed qemu-img to create a new virtual disk ("Ensure that you have installed qemu-img in your system and it's available").
+    quickemu # recommendation from Mastodon to create VMs
+    # quickgui # a GUI for guickemu - looked nice but doesn't support Windows hosts
     ];
 
 
