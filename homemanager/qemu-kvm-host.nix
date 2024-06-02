5529a801c735fa18b207d700ebaeafd209831bf2
Karl Voit <karl.voit@rise-world.com>
2024-06-02 19:55:15 +0200

    disabled virtman after compilation error

--- homemanager/qemu-kvm-host.nix
+++ homemanager/qemu-kvm-host.nix
@@ -4,7 +4,7 @@
 
     home.packages = with pkgs; [
     qemu
-    virt-manager-qt   ## seems to only connect to existing VMs and not create new ones
+    #virt-manager-qt   ## seems to only connect to existing VMs and not create new ones ## 2024-06-02: compile error with NixOS 24.05 → see id:2024-06-01-upgrade-jackson-to-NixOS-24-dot-05
     # broken:      aqemu
     # qtemu  ## looks nice but is not able to find the installed qemu-img to create a new virtual disk ("Ensure that you have installed qemu-img in your system and it's available").
     quickemu # recommendation from Mastodon to create VMs
