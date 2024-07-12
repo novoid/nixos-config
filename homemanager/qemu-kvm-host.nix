43c37d102b31a09703ab57260456ac05a47ae815
Karl Voit <karl.voit@rise-world.com>
2024-07-12 19:05:31 +0200

    disabled sshd on jackson

--- homemanager/qemu-kvm-host.nix
+++ homemanager/qemu-kvm-host.nix
@@ -9,6 +9,7 @@
     # qtemu  ## looks nice but is not able to find the installed qemu-img to create a new virtual disk ("Ensure that you have installed qemu-img in your system and it's available").
     quickemu # recommendation from Mastodon to create VMs
     # quickgui # a GUI for guickemu - looked nice but doesn't support Windows hosts
+    incus  # recommendation for GUI from STG
     ];
 
 
