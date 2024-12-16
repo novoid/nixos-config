d27b8a1a83a1f313856cf337fe7c4cd6f66edd5e
Karl Voit <karl.voit@rise-world.com>
2024-12-16 10:38:59 +0100

    ionice für nh

--- nh-os-switch.sh
+++ nh-os-switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # https://github.com/viperML/nh
-nice time nh os switch --ask "/home/vk/nixos"
+nice ionice time nh os switch --ask "/home/vk/nixos"
 
 notify-send "nh os switch" "finished"
 
