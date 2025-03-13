a751202c9d36c8d6300f3cbb26c6f8e7a4e1067d
Karl Voit <karl.voit@rise-world.com>
2025-03-13 11:11:22 +0100

    update-flake.sh: added HOST to commit messages

--- update-flake.sh
+++ update-flake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 nice nix flake update && \
 	git add flake.lock && \
-	git commit -m 'updated flake.lock via update-flake.sh' && \
+	git commit -m '${HOST}: updated flake.lock via update-flake.sh' && \
 	echo "finished without error."
 #end
