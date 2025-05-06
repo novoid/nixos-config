f09e42f7976ca9ab0db1944183c2da2227be45ba
Karl Voit <karl.voit@rise-world.com>
2025-05-06 09:15:29 +0200

    jackson: rustdesk as citrix

--- homemanager/business.nix
+++ homemanager/business.nix
@@ -14,6 +14,9 @@
       emacsPackages.helm-org-ql emacsPackages.helm-org emacsPackages.helm-org-rifle 
 
       nextcloud-client
+
+      #citrix_workspace ## 2025-05-06: package needs linuxx64-24.5.0.76.tar.gz but website only provides linuxx64-25.03.0.66.tar.gz
+      rustdesk ## Citrix alternative
     ];
 
 
